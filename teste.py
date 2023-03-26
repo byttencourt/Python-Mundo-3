aa09f4a01f39a90dfd7f7da97a5db8b434f7c7fc
byttencourt <byttencourt@hotmail.com>
2023-03-25 21:26:31 -0300

    v1

--- teste.py
+++ teste.py
@@ -1,6 +1,1 @@
-def find_max(nums):
-    max_num = float("-inf") # smaller than all other numbers
-    for num in nums:
-        if num > max_num:
-            print(max_num)
-            return max_num
+
